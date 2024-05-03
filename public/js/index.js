9c012d3ac943e239b14f5c54d5c4229da06ee6d3
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2024-05-03 21:44:50 +0800

    modify API

--- public/js/index.js
+++ public/js/index.js
@@ -15,7 +15,7 @@ const addFile = () => {
     console.log("innn");
     // 上傳 Image 檔案
     axios
-      .post(`https://4e52e77ee96a.ngrok.io/style`, formData, {
+      .post(`https://video-style-transfer-processor.onrender.com/style`, formData, {
         headers: {
           "content-type": "mutipart/form-data",
         },
@@ -28,7 +28,7 @@ const addFile = () => {
         videoPlayerRes.innerHTML = "";
         videoPlayerRes.classList.remove("d-none");
         const playerRes = videojs("videoPlayer-res", {
-          sources: [{ src: `https://4e52e77ee96a.ngrok.io/static/output.mp4` }],
+          sources: [{ src: `https://video-style-transfer-processor.onrender.com/static/output.mp4` }],
           loop: false,
           autoplay: "muted",
           controls: true,
